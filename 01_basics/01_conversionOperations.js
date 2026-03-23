1c6005d9732796c678a70ec80cb5442fbd5ff99f
gaurav13934u <kumargaurav13934u@gmail.com>
2026-03-23 11:55:29 +0000

    container config done

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//0 => false; 1 => true;
+//" "=> false
+//"GAURAV" => true
+
+
