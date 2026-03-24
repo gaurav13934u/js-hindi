f26b2fd65c26e131284ddfacc605dfd871dfae1e
gaurav13934u <kumargaurav13934u@gmail.com>
2026-03-24 13:52:58 +0000

    container config done

---
+++ 01_basics/01_comparisons.js
@@ -0,0 +1,21 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2<=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//console.log("2">1); it gives us unpredictabe result 
+//console.log("02">=1); this is not a good method to do the comparisons
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+// Note----> the reason is that an equality check == and comparisons >,<,>=,<= works differently.
+//Comparisons convert null to a number, treating it as 0.
+//thats why (3) null >= 0 is true and (1) null > 0 is false.
+
+// ===
+
+console.log("3" === 2);
+//we have to do clean code because its very important 
--- 01_basics/01_conversionOperations.js
+++ 01_basics/01_conversionOperations.js
@@ -4,8 +4,8 @@ console.log(typeof(score));
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 
@@ -14,14 +14,44 @@ console.log(valueInNumber);
 //"33abc" => NaN
 // true => 1; false => 0;
 
-let isLoggedIn = 1
+//let isLoggedIn = 1
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 
 //0 => false; 1 => true;
 //" "=> false
 //"GAURAV" => true
 
+//*******operations*********/
 
+let value = 3
+let negValue =-value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2); -->2 to the power 2
+//console.log(2/2);
+//console.log(2%2);remainder = 0;
+let str1 = "hello"
+let str2 = "Gaurav"
+let str3 = str1 + str2
+console.log(str3);
+
+//console.log(2+"2");---->22
+//console.log("2"+2);---->22
+//console.log("2"+2+3);-->223
+//console.log(2+2+"5");-->45
+
+//console.log((2+2) * 5 % 2);
+//console.log(2*2);
+//console.log(+true);-->1;
+ let num1, num2, num3 
+ num1=num2=num3= 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
